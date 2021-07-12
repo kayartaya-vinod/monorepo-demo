7520e65732d7e76bc8516963c4c244b60844a93d
Vinod Kumar Kayartaya <kayartaya.vinod@gmail.com>
2021-07-12 22:34:25 +0530

    first commit

---
+++ Jenkinsfile
@@ -0,0 +1,15 @@
+pipeline {
+    agent none
+    stages {
+        stage('Build services') {
+            steps {
+               dir (customer-service) {
+                sh 'mvn -B -DskipTests clean package'
+               }
+               dir (product-service) {
+                sh 'mvn -B -DskipTests clean package'
+               }
+            }
+        }
+    }
+}
---
+++ customer-service/src/main/java/com/kvinod/Application.java
@@ -0,0 +1,21 @@
+package com.kvinod;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@RestController
+@SpringBootApplication
+public class Application {
+
+	@GetMapping("/api/customers/info")
+	public String info() {
+		return "customer-service is here";
+	}
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+
+}
