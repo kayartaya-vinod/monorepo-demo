cf366d3bfadb7e72cbaa8d0d5c10e8205d34d9e7
Vinod Kumar Kayartaya <kayartaya.vinod@gmail.com>
2021-07-12 23:36:40 +0530

    change return type in /info

--- customer-service/src/main/java/com/kvinod/Application.java
+++ customer-service/src/main/java/com/kvinod/Application.java
@@ -4,14 +4,18 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.GetMapping;
+import java.util.Map;
+import java.util.HashMap;
 
 @RestController
 @SpringBootApplication
 public class Application {
 
 	@GetMapping("/api/customers/info")
-	public String info() {
-		return "customer-service is here";
+	public Map<String, Object> info() {
+		Map<String, Object> map = new HashMap<>();
+		map.put("message", "Customer Service is here");
+		return map;
 	}
 
 	public static void main(String[] args) {
